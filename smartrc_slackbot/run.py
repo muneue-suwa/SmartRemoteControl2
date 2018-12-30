ebff41063d3051dd9e123c0664e4fc8e522ae18e
Dongsik <dongsiku+git@gmail.com>
2018-12-30 22:36:53 +0900

    Add run.py of slackbot

---
+++ smartrc_slackbot/run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 22:36:06 2018
+
+@author: crantu
+"""
+
+from slackbot.bot import Bot
+
+
+def main():
+    bot = Bot()
+    bot.run()
+
+
+if __name__ == "__main__":
+    main()
