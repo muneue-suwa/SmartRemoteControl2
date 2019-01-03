85c1cbd53f4c43446bf6b30e9e2be03168d4de2e
Dongsik <dongsiku+git@gmail.com>
2019-01-03 23:18:29 +0900

    Add datetime to filename in record.py

--- src/record.py
+++ src/record.py
@@ -6,12 +6,17 @@ Created on Sun Dec 30 21:59:42 2018
 @author: crantu
 """
 
-from irrp_2 import IRRP2
+
 from os import path
+from datetime import datetime
+
+from irrp_2 import IRRP2
 
 
 def main(gpio_num, record_id, smartrc_dir):
-    filename = path.join(smartrc_dir, "data/irrp_2.json")
+    str_datetime = datetime.strftime(datetime.today(), "%Y%m%d_%H%M%S")
+    filename = path.join(smartrc_dir,
+                         "data/irrp_2_{}.json".format(str_datetime))
     irrp2 = IRRP2(PLAY=False, RECORD=True,
                   GPIO=gpio_num, FILE=filename, ID=record_id,
                   NO_CONFIRM=True, POST_MS=130)
