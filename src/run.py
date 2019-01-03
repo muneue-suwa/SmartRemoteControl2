4fd6646d4f21fe7fabe83c861744b5a55357923c
Dongsik <dongsiku+git@gmail.com>
2019-01-03 21:34:04 +0900

    Update run.py for rtm_connect of python-slackclient

--- src/run.py
+++ src/run.py
@@ -6,13 +6,27 @@ Created on Sun Dec 30 22:36:06 2018
 @author: crantu
 """
 
-from slackbot.bot import Bot
+
+from time import sleep
+from os import path
+
+from smartrc import SmartRemoteControl
 
 
-def main():
-    bot = Bot()
-    bot.run()
+class RunSmartrcBot(SmartRemoteControl):
+    def __init__(self, smartrc_dir):
+        super().__init__(smartrc_dir)
+
+    def main(self):
+        if self.sc.rtm_connect():
+            while self.sc.server.connected is True:
+                print(self.sc.rtm_read())
+                sleep(1)
+        else:
+            print("Connection Failed")
 
 
 if __name__ == "__main__":
-    main()
+    smartrc_dir = path.expanduser("~/Git/SmartRemoteControl2")
+    bot = RunSmartrcBot(smartrc_dir)
+    bot.main()
