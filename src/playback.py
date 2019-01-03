4d8b9e66bfbcf73e09fec4ab152d6ec9bd587c3c
Dongsik <dongsiku+git@gmail.com>
2019-01-04 00:00:02 +0900

    Add get_latest_irrp_2_json to playback.py and upload.py

--- src/playback.py
+++ src/playback.py
@@ -9,9 +9,11 @@ Created on Sun Dec 30 22:16:48 2018
 from irrp_2 import IRRP2
 from os import path
 
+from get_latest_irrp_2_json import get_latest_irrp_2_json
+
 
 def main(gpio_num, playback_id, smartrc_dir):
-    filename = path.join(smartrc_dir, "data/irrp_2.json")
+    filename = path.join(get_latest_irrp_2_json(smartrc_dir))
     irrp2 = IRRP2(PLAY=True, RECORD=False,
                   GPIO=gpio_num, FILE=filename, ID=playback_id)
     irrp2.main()
--- src/upload.py
+++ src/upload.py
@@ -9,11 +9,18 @@ Created on Mon Dec 31 22:22:08 2018
 import requests
 from os import path
 from datetime import datetime
+import json
+
+from get_latest_irrp_2_json import get_latest_irrp_2_json
 
 
 def main(slack_token, channel_id, smartrc_dir):
-    filename = path.join(smartrc_dir, "data/irrp_2.json")
-    str_datetime = datetime.strftime(datetime.today(), "%Y%m%d_%H%M%S")
+    filename = path.join(get_latest_irrp_2_json(smartrc_dir))
+    with open(filename, "r") as irrp_2:
+        json_file = json.load(irrp_2)
+        string = str(json_file).replace("'", '"').replace(" ", "")
+    print("len:", len(string))
+    print("string:", string)
 
     files = {'file': open(filename, 'r')}
     param = {
