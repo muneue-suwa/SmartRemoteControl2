ce20a1a7c6d8a2ea15d0174030343317171dd0bd
Dongsik <dongsiku+git@gmail.com>
2019-01-13 11:10:39 +0900

    Update run.py for IFTTT

--- src/run.py
+++ src/run.py
@@ -28,8 +28,10 @@ class RunSmartrcBot(SmartRemoteControl):
             while self.sc.server.connected is True:
                 try:
                     msg = self.sc.rtm_read()
-                    print("msg:", msg[0]["text"])
+                    print("msg_human:", msg[0]["text"])
+                    print("msg_bot:", msg[0]["attachments"][0]["fallback"])
                     self.analyze_message(msg[0]["text"])
+                    self.analyze_message(msg[0]["attachments"][0]["fallback"])
                 except IndexError:
                     pass
                 except KeyError:
