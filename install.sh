0b36bec6c31c71f5334b032481402faf7c66ca8b
Dongsik <dongsiku+git@gmail.com>
2019-01-14 09:22:22 +0900

    Add installation/ and move crontab to it

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Get path to this file
 INSTALL_SH_FILENAME=`readlink -f $0`
 INSTALL_SH_DIRNAME=`dirname $INSTALL_SH_FILENAME`
 
+# Make directories for log and data
 mkdir -p $INSTALL_SH_DIRNAME/log
 mkdir -p $INSTALL_SH_DIRNAME/data
 
-python3 $INSTALL_SH_DIRNAME/crontab/make_crontab.py $INSTALL_SH_DIRNAME
-crontab $INSTALL_SH_DIRNAME/crontab/smartrc_bot.crontab
+# Install crontab
+python3 $INSTALL_SH_DIRNAME/installation/make_crontab.py $INSTALL_SH_DIRNAME
+crontab $INSTALL_SH_DIRNAME/installation/smartrc_bot.crontab
+
+# Install smartrc
