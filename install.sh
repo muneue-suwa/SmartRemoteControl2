a255cdf2f45a4b38c46001c60049507f998d5929
Dongsik <dongsiku+git@gmail.com>
2019-09-12 23:21:44 +0900

    Update install.sh: add the version of the pigpio package

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ installation()
   # Install
   python3 $INSTALL_SH_DIRNAME/installation/make_installation_files.py $INSTALL_SH_DIRNAME
   ## Additional APT
-  sudo apt install pigpio -y
+  sudo apt install pigpio=1.71-0~rpt1 -y
   ## pigpio
   sudo pigpiod
   sudo systemctl enable pigpiod.service
