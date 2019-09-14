0428ba091a74f5a7ce3d74240afa846c8ee11228
Dongsik <dongsiku+git@gmail.com>
2019-09-14 12:08:35 +0900

    Update install.sh and uninstall.sh to check setting/.smartrc.cfg is removed before installation

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Get path to this file
+INSTALL_SH_FILENAME=`readlink -f $0`
+INSTALL_SH_DIRNAME=`dirname $INSTALL_SH_FILENAME`
+
+# Remove setting/.smartrc.cfg
+rm $INSTALL_SH_DIRNAME/setting/.smartrc.cfg
+
 # Uninstall crontab
 crontab -r
 sudo crontab -r
